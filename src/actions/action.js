3ae01825e136e00cfea335064aa22003ffa133f9
steveyalcin <steven.yalcin@gmail.com>
2020-03-29 21:16:04 -0300

    The reducers are commented out

--- src/actions/action.js
+++ src/actions/action.js
@@ -1,11 +1,13 @@
 // ACTION => THE ACTION THAT YOU WANNA IMPLEMENT
 
+// set the increment action
 export const increment = () => {
   return {
     type: 'INCREMENT'
   };
 };
 
+// set the decrement action
 export const decrement = () => {
   return {
     type: 'DECREMENT'
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,7 @@ import counterReducer from './counter';
 import loggedReducer from './isLogged';
 import { combineReducers } from 'redux';
 
+// you can combine all reducers with the method
 const allReducer = combineReducers({
   counter: counterReducer,
   login: loggedReducer
